59f2ea5f80a6274b1fa28eb14825773075ca3621
Echochuan <912297261@qq.com>
2021-07-06 19:56:49 +0800

    :bug: 重新定义创建二叉树的函数

--- 大二小学期/smallTerm.cpp
+++ 大二小学期/smallTerm.cpp
@@ -1,27 +1,29 @@
 //假设二叉树采用链式存储方式，编写一个程序，判断任意给定的二叉树是否为满二叉树。
 //一个满二叉树总结点树等于2的最大深度次方-1。
+//最大深度是通过层次遍历求出
 
 #include <iostream>
 using namespace std;
 
 //声明二叉树结构
-typedef struct BiTnode {
+struct BiTree {
     int data;   //数值域
-    struct BiTnode *Lchild,*Rchild;     //指针域
-}BiTnode,*BiTree;
+    BiTree *Lchild;
+    BiTree *Rchild;     //指针域
+};
 
 //设定创建二叉树的函数
-void CreateBiTree(BiTree &T) {
-    char ch;
-    char temp;
-    cin >> ch;  //输入数据
-    temp = getchar();
-    if(ch == '#') {
+void CreateBiTree(BiTree * &T) {
+    int d;
+    cin >> d;  //输入数据
+
+    //当输入-1时为空结点
+    if(d == -1) {
         T = NULL;
     }
     else {
-        T = new BiTnode;            //新建一个节点
-        T->data = ch;             //该节点的数据域为输入的内容
+        T = (BiTree *)malloc(sizeof(BiTree));   //使用sizeof计算字节数，使用malloc动态分配内存空间
+        T->data = d;             //该节点的数据域为输入的内容
         CreateBiTree(T->Lchild);    //递归创建左子树
         CreateBiTree(T->Rchild);    //递归创建右子树
     }
@@ -29,7 +31,7 @@ void CreateBiTree(BiTree &T) {
 
 
 //判断是否为满二叉树
-int isFull(BiTree T){
+bool isFull(BiTree T){
     //如果左孩子与右孩子同时为空，则该树为满二叉树
     if(T->Lchild == NULL && T->Rchild == NULL ) {
         return 1;
