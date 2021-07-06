6d316f3697bc5d82feb5996fe353d8b5fbd76e31
Echochuan <912297261@qq.com>
2021-07-06 19:07:19 +0800

    :sparkles: 声明二叉树结构

--- 大二小学期/smallTerm.cpp
+++ 大二小学期/smallTerm.cpp
@@ -1,1 +1,11 @@
 //假设二叉树采用链式存储方式，编写一个程序，判断任意给定的二叉树是否为满二叉树。
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//声明二叉树结构
+typedef struct BiTnode {
+    int data;   //数值域
+    struct BiTnode *Lchild,*Rchild;     //指针域
+}BiTnode,*BiTree;
