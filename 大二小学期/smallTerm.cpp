b83bf632d9dc82cca23ee1ef733ed8b06dd41dd9
Echochuan <912297261@qq.com>
2021-07-06 20:19:41 +0800

    :sparkles: 重构主函数

--- 大二小学期/smallTerm.cpp
+++ 大二小学期/smallTerm.cpp
@@ -65,6 +65,14 @@ bool isFull(BiTree * T){
 
 //主函数
 int main() {
+    BiTree *T;
+    cout << "使用先序遍历的方法输入结点数据，当输入-1时表示该结点为空 ";
+    CreateBiTree(T);
+    cout << "建好的树输出如下 ";
+    cout << T;
+    string a = isFull(T) ? "是" : "不是";
+    cout << "该二叉树 " << a << "满二叉树";
 
+    return 0;
 }
 
